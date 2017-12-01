f74cd2e42baf95b13b73c3b1a6dfb0d9839b9939
beccyo3o <rebecca.krummel@gmail.com>
2017-12-01 17:14:38 +0100

    add items in cart state and pruducts styles

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,39 @@ import React, { Component } from 'react'
 import ProductList from './product-list'
 import ShoppingCart from './shopping-cart'
 
-import '../styles/App.css'
+import '../styles/app.css'
+import '../styles/cart.css'
+import '../styles/products.css'
 
 class App extends Component {
+
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      itemsInCart: {}
+    }
+    this.addToCart = this.addToCart.bind(this)
+  }
+
+  addToCart(id, name, price) {
+    this.setState({
+      itemsInCart: {
+        id: id,
+        name: name,
+        price: price
+      }
+    })
+  }
+
   render() {
     return (
       <div>
         <header className="App-header">
-          test header
+          Shopping Cart Calculator
         </header>
-        <ProductList />
-        <ShoppingCart />
+        <ProductList onClick={this.addToCart}/>
+        <ShoppingCart itemsInCart={this.state.itemsInCart}/>
       </div>
     )
   }
